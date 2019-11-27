c4f74812b8d87878c9c99e4e342cbc4079db21e9
Jason Sowers <jason.w.sowers@gmail.com>
2019-11-27 13:20:07 -0800

    added stats

--- BITCORNService/Controllers/TxController.cs
+++ BITCORNService/Controllers/TxController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BITCORNService.Models;
+using BITCORNService.Utils;
+using BITCORNService.Utils.LockUser;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BITCORNService.Controllers
 {
+    [LockUser]
     [Route("api/[controller]")]
     [ApiController]
     public class TxController : ControllerBase
@@ -21,6 +25,43 @@ namespace BITCORNService.Controllers
         public void Tipcorn([FromBody] string value)
         {
 
+        }
+        async Task HandleAnalytics(SendFromRequest request, decimal amount)
+        {
+            using (var db = new BitcornContext())
+            {
+                var user = PrepareUser(db, request.GetRequestUser());
+                user.Tips += 1;
+
+                if (user.Tiptotal == 0)
+                {
+                    user.Tiptotal = amount;
+                }
+                else
+                {
+                    user.Tiptotal += amount;
+                }
+
+                try
+                {
+                    await db.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    BITCORNLogger.LogError(e, $"Tip or Tiptotal update failed to save to Database ");
+                }
+
+            }
+        }
+
+        private Users PrepareUser(BitcornContext db, Users old)
+        {
+            var entry = db.Attach(old);
+
+            entry.Property(e => e.Tips).IsModified = true;
+            entry.Property(e => e.Tiptotal).IsModified = true;
+
+            return old;
         }
 
     }
---
+++ BITCORNService/Utils/Stats/UpdateStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using BITCORNService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BITCORNService.Utils.Stats
+{
+    public static class UpdateStats
+    {
+        public static async Task Tip(int userId, decimal amount)
+        {
+            using (var dbContext = new BitcornContext())
+            {
+                var userStats = await dbContext.UserStat.FirstOrDefaultAsync(u => u.UserId == userId);
+                userStats.Tip += 1;
+                userStats.TipTotal += amount;
+            }
+        }
+
+        public static async Task Tipped()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
