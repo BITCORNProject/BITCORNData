7131a952eb2c350702d2354efae833ff99faf501
Samuli Salonen <salonen.samuli1@gmail.com>
2019-11-27 23:48:17 +0200

    Refactor wallet output objects

---
+++ BITCORNService/Utils/Wallet/Models/BlockchainTX.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BITCORNService.Utils.Wallet.Models
+{
+    /// <summary>
+    /// Deserialized Transaction info response returned by the gettransaction call
+    /// </summary>
+    [Serializable]
+    public class BlockchainTX
+    {
+        [JsonProperty("amount")]
+        public double Amount { get; set; }
+        [JsonProperty("confirmations")]
+        public double Confirmations { get; set; }
+        [JsonProperty("generated")]
+        public bool Generated { get; set; }
+        [JsonProperty("blockhash")]
+        public string Blockhash { get; set; }
+        [JsonProperty("blockindex")]
+        public double blockindex { get; set; }
+        [JsonProperty("blocktime")]
+        public long Blocktime { get; set; }
+        [JsonProperty("txid")]
+        public string TxId { get; set; }
+        [JsonProperty("walletconflicts")]
+        public object[] WalletConflicts { get; set; }
+        [JsonProperty("time")]
+        public long Time { get; set; }
+        [JsonProperty("timereceived")]
+        public long TimeReceived { get; set; }
+        [JsonProperty("details")]
+        public BlockchainTxDetail[] Details { get; set; }
+        [JsonProperty("hex")]
+        public string Hex { get; set; }
+
+    }
+}
+
