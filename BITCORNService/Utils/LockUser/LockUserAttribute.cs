3ac3160625e96cb7d8d6f7ad614859a1919b2414
Jason Sowers <jason.w.sowers@gmail.com>
2019-11-27 11:45:10 -0800

    added LockUserAttribute. updates

---
+++ BITCORNService/Utils/LockUser/LockUserAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+
+namespace BITCORNService.Utils
+{
+    public class LockUserAttribute : ActionFilterAttribute
+    {
+        public static HashSet<string> LockedUsers = new HashSet<string>();
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Request.Headers.TryGetValue("platform", out StringValues platform); 
+            context.HttpContext.Request.Headers.TryGetValue("id", out StringValues id);
+
+            base.OnActionExecuting(context);
+        }
+
+
+    }
+}
