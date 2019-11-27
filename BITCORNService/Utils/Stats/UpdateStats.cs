d31c323f285201526eeb552d2e12a9c4ffaad77e
Jason Sowers <jason.w.sowers@gmail.com>
2019-11-27 13:44:50 -0800

    added tipped method to updatestats

--- BITCORNService/Utils/Stats/UpdateStats.cs
+++ BITCORNService/Utils/Stats/UpdateStats.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using BITCORNService.Models;
+using BITCORNService.Utils.DbActions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace BITCORNService.Utils.Stats
 {
@@ -17,12 +19,28 @@ namespace BITCORNService.Utils.Stats
                 var userStats = await dbContext.UserStat.FirstOrDefaultAsync(u => u.UserId == userId);
                 userStats.Tip += 1;
                 userStats.TipTotal += amount;
+                if (userStats.TopTip < amount)
+                {
+                    userStats.TopTip = amount;
+                }
+
+                await dbContext.SaveAsync();
             }
         }
 
-        public static async Task Tipped()
+        public static async Task Tipped(int userId, decimal amount)
         {
-            throw new NotImplementedException();
+            using (var dbContext = new BitcornContext())
+            {
+                var userStats = await dbContext.UserStat.FirstOrDefaultAsync(u => u.UserId == userId);
+                userStats.Tipped += 1;
+                userStats.TippedTotal += amount;
+                if (userStats.TopTiped < amount)
+                {
+                    userStats.TopTiped = amount;
+                }
+                await dbContext.SaveAsync();
+            }
         }
 
     }
