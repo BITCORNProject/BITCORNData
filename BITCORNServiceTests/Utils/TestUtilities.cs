da92d43dbd8c531a9b5f541a42a8480ee362b0b8
Samuli Salonen <salonen.samuli1@gmail.com>
2019-11-29 21:56:28 +0200

    Add Test utils

---
+++ BITCORNServiceTests/Utils/TestUtilities.cs
@@ -0,0 +1,51 @@
+﻿using BITCORNService;
+using BITCORNService.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BITCORNServiceTests.Utils
+{
+    public static class TestUtilities
+    {
+        public static BitcornContext CreateInMemoryDatabase()
+        {
+           var dbOptions = new DbContextOptionsBuilder<BitcornContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options; 
+
+            return new BitcornContext(dbOptions);
+          
+        }
+        public static IConfigurationRoot GetConfig()
+        {
+            return new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        }
+        public static BitcornContext CreateDatabase()
+        {
+            var dbOptions = new DbContextOptionsBuilder<BitcornContext>()
+               .UseSqlServer(GetConfig()["Config:ConnectionString"])
+               .Options;
+
+            return new BitcornContext(dbOptions);
+
+        }
+        public static void AddUser(this BitcornContext context, UserIdentity identity,UserWallet wallet)
+        {
+            var user = new User();
+            context.User.Add(user);
+            context.SaveChanges();
+            
+            identity.UserId = user.UserId;
+            wallet.UserId = user.UserId;
+
+            context.UserIdentity.Add(identity);
+            context.UserWallet.Add(wallet);
+            context.SaveChanges();
+
+        }
+    }
+}
