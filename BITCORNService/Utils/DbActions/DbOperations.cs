53653cb619c88cb4e6a01728d0fd5de178f19bb4
Samuli <salonen.samuli1@gmail.com>
2019-12-22 20:38:12 +0200

    allow passing db transaction to the save method

--- BITCORNService/Utils/DbActions/DbOperations.cs
+++ BITCORNService/Utils/DbActions/DbOperations.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Threading.Tasks;
 using BITCORNService.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Serilog;
 
 namespace BITCORNService.Utils.DbActions
@@ -43,27 +44,25 @@ namespace BITCORNService.Utils.DbActions
             return await dbContext.CornTx.AnyAsync(w => w.BlockchainTxId == txId);
         }
 
-        public static async Task SaveAsync(this BitcornContext dbContext)
+        public static async Task SaveAsync(this BitcornContext dbContext, IDbContextTransaction transaction)
         {
 
             //create execution strategy so the request can retry if it fails to connect to the database
             var strategy = dbContext.Database.CreateExecutionStrategy();
             await strategy.ExecuteAsync(async () =>
             {
-                using (var transaction = dbContext.Database.BeginTransaction(IsolationLevel.RepeatableRead))
+                try
                 {
-                    try
-                    {
-                        await dbContext.SaveChangesAsync();
-                        transaction.Commit();
-                    }
-                    catch (Exception e)
-                    {
-                        transaction.Rollback();
-                        Log.Logger.Error(e.Message,e);
-                        throw new Exception(e.Message, e.InnerException);
-                    }
+                    await dbContext.SaveChangesAsync();
+                    transaction?.Commit();
                 }
+                catch (Exception e)
+                {
+                    transaction?.Rollback();
+                    Log.Logger.Error(e.Message, e);
+                    throw new Exception(e.Message, e.InnerException);
+                }
+
             });
 
         }
