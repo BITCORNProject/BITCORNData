cdae232fcbeb51c475d9781389282414d166821a
Samuli <salonen.samuli1@gmail.com>
2019-12-25 03:29:56 +0200

    Modify rain request model

---
+++ BITCORNService/Utils/Models/RainRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BITCORNService.Utils.Models
+{
+    public class RainRequest : ITxRequest
+    {
+        public string From { get; set; }
+        public decimal Amount { get; set; }
+        public string Platform { get; set; }
+        public string[] To { get; set; }
+        public string[] Columns { get;}
+        string ITxRequest.TxType => "$rain";
+        IEnumerable<string> ITxRequest.To => this.To;
+    }
+}
