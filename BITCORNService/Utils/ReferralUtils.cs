c2eb84aded3fad7c451e9925d0a0535e9bf572bc
Jason Sowers <jason.w.sowers@gmail.com>
2020-02-22 14:46:43 -0800

    updates to referral system

--- BITCORNService/Utils/ReferralUtils.cs
+++ BITCORNService/Utils/ReferralUtils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BITCORNService.Models;
+using BITCORNService.Utils.DbActions;
 using BITCORNService.Utils.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,44 +11,29 @@ namespace BITCORNService.Utils
 {
     public static class ReferralUtils
     {
-        public static void UpdateReferralSync(BitcornContext dbContext, PlatformId platformId)
+        public static async Task UpdateReferralSync(BitcornContext dbContext, PlatformId platformId)
         {
-            int? userId =0;
-            switch (platformId.Platform)
-            {
-                case "twitch":
-                    userId = dbContext.UserIdentity.FirstOrDefault(u => platformId != null && u.TwitchId == platformId.Id)?.UserId;
-                    break;
-                case "twitter":
-                    userId = dbContext.UserIdentity.FirstOrDefault(u => platformId != null && u.TwitterId == platformId.Id)?.UserId;
-                    break;
-                case "discord":
-                    userId = dbContext.UserIdentity.FirstOrDefault(u => platformId != null && u.DiscordId == platformId.Id)?.UserId;
-                    break;
-                case "reddit":
-                    userId = dbContext.UserIdentity.FirstOrDefault(u => platformId != null && u.RedditId == platformId.Id)?.UserId;
-                    break;
-            }
+            int? userId = BitcornUtils.GetUserForPlatform(platformId, dbContext).Select(u => u.UserId).FirstOrDefault();
 
             if (userId != 0 && userId != null)
             {
-                var userReferral = dbContext.UserReferral.FirstOrDefault(u => u.UserId == userId);
+                var userReferral = await dbContext.UserReferral.FirstOrDefaultAsync(u => u.UserId == userId);
                 if (userReferral != null && userReferral.SyncDate == null)
                 {
-                    var referrer =
-                        dbContext.Referrer.FirstOrDefault(r => r.ReferralId == userReferral.ReferralId);
-                    var referrerWallet = dbContext.UserWallet.FirstOrDefault(w => w.UserId == referrer.UserId);
+                    var referrer = await 
+                        dbContext.Referrer.FirstOrDefaultAsync(r => r.ReferralId == userReferral.ReferralId);
+                    var referrerWallet = await dbContext.UserWallet.FirstOrDefaultAsync(w => w.UserId == referrer.UserId);
                     if (referrer != null)
                         if (referrerWallet != null)
                             referrerWallet.Balance += referrer.Amount;
 
-                    var botWallet = dbContext.UserWallet.FirstOrDefault(w => w.UserId == 196);
+                    var botWallet = await dbContext.UserWallet.FirstOrDefaultAsync(w => w.UserId == 196);
                     if (botWallet != null)
                     {
                         if (referrer != null)
                         {
                             botWallet.Balance -= referrer.Amount;
-                            var stats = dbContext.UserStat.FirstOrDefault(s => s.UserId == referrer.UserId);
+                            var stats = await dbContext.UserStat.FirstOrDefaultAsync(s => s.UserId == referrer.UserId);
                             if (stats != null)
                             {
                                 stats.TotalReferralRewards += referrer.Amount;
@@ -56,6 +42,8 @@ namespace BITCORNService.Utils
                     }
                     userReferral.SyncDate = DateTime.Now;
                 }
+
+                await dbContext.SaveAsync();
             }
         }
     }
