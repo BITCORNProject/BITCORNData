82261b18b1947a588302237ed8e6872c6e22b041
Samuli <salonen.samuli1@gmail.com>
2020-08-22 23:28:42 +0300

    authscopes

---
+++ BITCORNService/Utils/Auth/AuthScopes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BITCORNService.Utils.Auth
+{
+    public class AuthScopes
+    {
+        public const string Withdraw = "transaction:withdraw";
+        public const string Deposit = "transaction:deposit";
+        public const string SendTransaction = "transaction:send";
+        public const string ReadTransaction = "transaction:read";
+        public const string ChangeUser = "user:change";
+        public const string AddUser = "user:add";
+        public const string BanUser = "user:ban";
+
+    }
+}
---
+++ BITCORNService/Utils/Auth/RequireScope.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+
+namespace BITCORNService.Utils.Auth
+{
+    public class RequireScope : IAuthorizationRequirement
+    {
+        public string Issuer { get; }
+        public string Scope { get; }
+
+        public RequireScope(string scope, string issuer)
+        {
+            Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+            Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
+        }
+    }
+}
